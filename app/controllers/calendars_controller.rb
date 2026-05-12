d7f9820e2d3b5a471ac18c0b2336d836e2dfe0c8
watanabedaiju0613-cloud <watanabe.daiju.0613@gmail.com>
2026-05-12 20:50:47 +0900

    first commit

---
+++ app/controllers/calendars_controller.rb
@@ -0,0 +1,35 @@
+class CalendarsController < ApplicationController
+
+  def index
+    get_week
+    @plan = Plan.new
+  end
+
+  def create
+    Plan.create(plan_params)
+    redirect_to action: :index
+  end
+
+  private
+
+  def plan_params
+    params.require(:calendars).permit(:date, :plan)
+  end
+
+  def get_week
+    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+
+    @todays_date = Date.today
+
+    @week_days = []
+    plans = Plan.where(date: @todays_date..@todays_date + 6)
+    7.times do |x|
+      today_plans = []
+      plans.each do |plan|
+        today_plans.push(plan.plan) if plan.date == @todays_date + x
+      end
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
+      @week_days.push(days)
+    end
+  end
+end
